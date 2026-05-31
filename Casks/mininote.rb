40f7aa02b36acb002cce95c2e82017a258d8d78b
vivalucas <jdliu6@gmail.com>
2026-05-31 09:12:30 +0800

    Update MiniNote cask to 0.4.4

--- Casks/mininote.rb
+++ Casks/mininote.rb
@@ -1,6 +1,6 @@
 cask "mininote" do
-  version "0.4.3"
-  sha256 "8d0a134e5781948fe77a3ad1bd93ef1dff6b9864ae9f34acc9e1971ae4c26689"
+  version "0.4.4"
+  sha256 "6841c712fe102c57b56704ce1903f22127855ba31bd45d3b76592e85b52b94b6"
 
   url "https://github.com/vivalucas/mininote/releases/download/v#{version}/MiniNote-#{version}.dmg"
   name "MiniNote"
