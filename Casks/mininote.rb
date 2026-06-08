71dc4c816bcad236ca6db10b27f1cf1065cabefd
vivalucas <jdliu6@gmail.com>
2026-06-08 11:12:08 +0800

    Update MiniNote cask to 0.9.0

--- Casks/mininote.rb
+++ Casks/mininote.rb
@@ -1,6 +1,6 @@
 cask "mininote" do
-  version "0.7.1"
-  sha256 "98054935ee3cb9da8390c094474e2caab4acb25c4f7087b5884a5a1ff9e9f4b5"
+  version "0.9.0"
+  sha256 "3d3c9298baacd3fd5188bd9e943ccfd7bfcf507375ad6aec584f29fede1146b6"
 
   url "https://github.com/vivalucas/mininote/releases/download/v#{version}/mininote-#{version}-macos-arm64.dmg"
   name "MiniNote"
